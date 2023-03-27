f009ef5efee3b51ee3586ac81164c119da09faa4
danil <musikhin.danil@gmail.com>
2023-03-27 21:17:10 +0500

    Some changes for better experience.

--- Scripts/HealthBar/HealthBar.cs
+++ Scripts/HealthBar/HealthBar.cs
@@ -8,51 +8,51 @@ using System;
 /// После добавления привязки демонстрацию нужно удалить (это все поля данного класса и функция _Ready)
 /// </summary>
 public class HealthBar : TextureProgress
-{	
-  private int HPFake = 2;                     //Фейковое ХП(для демонстрации работы метода UpdateHealthBar)
-  private int HPFakeMax = 500;                //Фейковое максимальное ХП(для демонстрации работы метода UpdateHealthBar)
-  private bool isHPFakeIncreasing = true;     //Переменная, обеспечивающая переход текущего хп от 0 до максимального
+{
+    private int HPFake = 2;                     //Фейковое ХП(для демонстрации работы метода UpdateHealthBar)
+    private int HPFakeMax = 500;                //Фейковое максимальное ХП(для демонстрации работы метода UpdateHealthBar)
+    private bool isHPFakeIncreasing = true;     //Переменная, обеспечивающая переход текущего хп от 0 до максимального
 
 
-  public override void _Ready()
-  {
-    //Вот здесь нужно добавить получение сведений о хп персонажа из другого источника
-  }
-  
-  
-  /// <summary>
-  /// Функция обновляет полоску ХП в соответствии с введёнными в неё значениями
-  /// </summary>
-  /// <curHP>Текущее хп</curHP>
-  /// <maxHP>Максимальное ХП</maxHP>
-  public void UpdateHealthBar(int curHP,int maxHP)
-  {
-    this.Value=(int)((double)curHP/maxHP*100);                 //Обновить рисунок полоски
-    ((Label)GetNode("HealthLabel")).Text=$"{curHP}/{maxHP}";   //Обновить текст полоски
-  }
+    public override void _Ready()
+    {
+        //Вот здесь нужно добавить получение сведений о хп персонажа из другого источника
+    }
 
 
-  
-   public override void _Process(float delta)
-   {
-     UpdateHealthBar(HPFake,HPFakeMax);     //Обновление полоски ХП в соответствии с фейк-значениями
-     
-     //Изменение фейк-значений таким образом, чтобы текущее хп изменялось плавно от 0 до максимума и наоборот
-     if(isHPFakeIncreasing)
-     {
-        HPFake+=1;
-        if(HPFake>=HPFakeMax)
+    /// <summary>
+    /// Функция обновляет полоску ХП в соответствии с введёнными в неё значениями
+    /// </summary>
+    /// <curHP>Текущее хп</curHP>
+    /// <maxHP>Максимальное ХП</maxHP>
+    public void UpdateHealthBar(int curHP, int maxHP)
+    {
+        this.Value = (int)((double)curHP / maxHP * 100);                 //Обновить рисунок полоски
+        ((Label)GetNode("HealthLabel")).Text = $"{curHP}/{maxHP}";   //Обновить текст полоски
+    }
+
+
+
+    public override void _Process(float delta)
+    {
+        UpdateHealthBar(HPFake, HPFakeMax);     //Обновление полоски ХП в соответствии с фейк-значениями
+
+        //Изменение фейк-значений таким образом, чтобы текущее хп изменялось плавно от 0 до максимума и наоборот
+        if (isHPFakeIncreasing)
         {
-          isHPFakeIncreasing=false;
+            HPFake += 1;
+            if (HPFake >= HPFakeMax)
+            {
+                isHPFakeIncreasing = false;
+            }
         }
-      }
-      else
-      {
-        HPFake-=1;
-        if(HPFake<=0)
+        else
         {
-          isHPFakeIncreasing=true;
+            HPFake -= 1;
+            if (HPFake <= 0)
+            {
+                isHPFakeIncreasing = true;
+            }
         }
-      }
     }
 }
